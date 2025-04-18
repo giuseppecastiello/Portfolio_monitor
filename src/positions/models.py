434c151bc7c6a39fcb036e29b1ffd59f84d2133d
giuseppecastiello <giuseppecastiello@outlook.com>
2025-04-18 11:45:27 +0200

    updated position model and schemas

--- src/positions/models.py
+++ src/positions/models.py
@@ -1,6 +1,12 @@
-from sqlalchemy import ForeignKey
+from datetime import date as pydate
+from enum import Enum as pyEnum
+from sqlalchemy import ForeignKey, Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.database import Base
+
+class TypeEnum(str, pyEnum):
+    buy = "b"
+    sell = "s"
 
 class Position(Base):
     __tablename__ = "positions"
@@ -10,3 +16,6 @@ class Position(Base):
     company_ticker: Mapped[str] = mapped_column(ForeignKey("companies.ticker", ondelete="CASCADE", onupdate="CASCADE"), primary_key=True, index=True)
     company = relationship("Company", back_populates="positions")
     quantity: Mapped[int] = mapped_column(nullable=False)
+    date: Mapped[pydate] = mapped_column(nullable=False)
+    price: Mapped[float] = mapped_column(nullable=False)
+    type: Mapped[TypeEnum] = mapped_column(Enum(TypeEnum, name="typeenum"), nullable=False)
